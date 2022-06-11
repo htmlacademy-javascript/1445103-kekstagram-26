ab4fe4f40786de7ebf0fe4dc5a810970544df691
Valeria <lerika.konstantinova@gmail.com>
2022-06-11 22:26:02 +1000

    Первые функции

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// Функция проверки длины введённого комментария
+
+const checkLength = function (line, maxLength) {
+  if (line <= maxLength) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+checkLength ('Hello', 15);
+
+//Функция, возвращающая случайное целое число из переданного диапазона
+
+const getRandomDiceNumber = function (min, max) {
+  if (min >= 0 || max >= 0) {
+    return Math.floor(Math.random() * max);
+  } else {
+    return false;
+  }
+};
+
+getRandomDiceNumber (7, 12);
