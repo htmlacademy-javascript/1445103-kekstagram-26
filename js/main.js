0811a396a0cf7f3d730cce59988043d0a2995226
Valeria <lerika.konstantinova@gmail.com>
2022-06-13 00:11:26 +1000

    Функции исправления

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 // Функция проверки длины введённого комментария
 
 const checkLength = function (line, maxLength) {
-  if (line <= maxLength) {
+  if (line.length <= maxLength) {
     return true;
-  } else {
-    return false;
   }
 };
 
@@ -14,7 +12,7 @@ checkLength ('Hello', 15);
 
 const getRandomDiceNumber = function (min, max) {
   if (min >= 0 || max >= 0) {
-    return Math.floor(Math.random() * max);
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
   } else {
     return false;
   }
